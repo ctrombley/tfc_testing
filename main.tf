64c5320b270219072c2950ce49cf1150bc561a5d
Chris Trombley <chris.trombley@hashicorp.com>
2023-04-11 15:33:43 -0700

    provider cfg

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
+  region = "us-east-1"
 }
 
 resource "aws_rds_cluster" "example" {
