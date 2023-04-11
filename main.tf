840320c5ea891de4bde7cc71dd362b8d7f417b8d
Chris Trombley <chris.trombley@hashicorp.com>
2023-04-11 14:39:10 -0700

    initial checkin

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+}
+
+resource "aws_rds_cluster" "example" {
+  cluster_identifier = "example"
+  engine             = "aurora-postgresql"
+  engine_mode        = "provisioned"
+  engine_version     = "14.6"
+  database_name      = "test"
+  master_username    = "test"
+  master_password    = "must_be_eight_characters"
+
+  serverlessv2_scaling_configuration {
+    max_capacity = 1.0
+    min_capacity = 0.5
+  }
+}
+
+resource "aws_rds_cluster_instance" "example" {
+  cluster_identifier = aws_rds_cluster.example.id
+  instance_class     = "db.serverless"
+  engine             = aws_rds_cluster.example.engine
+  engine_version     = aws_rds_cluster.example.engine_version
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "hostname" {
+  type = string
+}
+
+variable "organization" {
+  type = string
+}
+
+variable "random_var" {
+  type = string
+  default = "test_Var"
+}
