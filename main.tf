52b912ffc477f03cb4ed3d75f784bf5641016245
Chris Trombley <chris.trombley@hashicorp.com>
2023-05-01 17:52:34 -0700

    plantimestamp

--- main.tf
+++ main.tf
@@ -7,53 +7,13 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_rds_cluster" "example" {
-  cluster_identifier = "example"
-  engine             = "aurora-postgresql"
-  engine_mode        = "provisioned"
-  engine_version     = "14.6"
-  database_name      = "test"
-  master_username    = "test"
-  master_password    = "must_be_eight_characters"
-
-  serverlessv2_scaling_configuration {
-    max_capacity = 1.0
-    min_capacity = 0.5
-  }
-}
-
-resource "aws_rds_cluster_instance" "example" {
-  cluster_identifier = aws_rds_cluster.example.id
-  instance_class     = "db.serverless"
-  engine             = aws_rds_cluster.example.engine
-  engine_version     = aws_rds_cluster.example.engine_version
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+check "terraform_io_certificate" {
+  data "tls_certificate" "terraform_io" {
+    url = "https://www.terraform.io/"
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
+  assert {
+    condition = timecmp(plantimestamp(), data.tls_certificate.terraform_io.certificates[0].not_after) < 0
+    error_message = "terraform.io certificate has expired"
   }
 }
