e8bdda4a09e8a36bc6a7be63e64a3f212ab2e114
Chris Trombley <chris.trombley@hashicorp.com>
2023-05-04 16:58:17 -0700

    add lots of vms

--- mainer.tf
+++ mainer.tf
@@ -64,7 +64,9 @@ resource "azurerm_network_security_group" "pub" {
 }
 
 resource "azurerm_network_security_rule" "pub_deny" {
-  name                        = "Deny-VNet-out"
+  count = 100 
+
+  name                        = "Deny-VNet-out ${count.index}"
   priority                    = 4000
   direction                   = "Outbound"
   access                      = "Deny"
@@ -80,4 +82,52 @@ resource "azurerm_network_security_rule" "pub_deny" {
 resource "azurerm_subnet_network_security_group_association" "pub" {
   subnet_id                 = azurerm_subnet.pub.id
   network_security_group_id = azurerm_network_security_group.pub.id
+}
+
+resource "azurerm_network_interface" "main" {
+  count = 100 
+
+  name                = "$test-nic-${count.index}"
+  location            = azurerm_resource_group.vnet.location
+  resource_group_name = azurerm_resource_group.vnet.name
+
+  ip_configuration {
+    name                          = "testconfiguration${count.index}"
+    subnet_id                     = azurerm_subnet.pub.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+resource "azurerm_virtual_machine" "main" {
+  count = 100 
+
+  name                  = "test-vm-${count.index}"
+  location              = azurerm_resource_group.vnet.location
+  resource_group_name   = azurerm_resource_group.vnet.name
+  network_interface_ids = [azurerm_network_interface.main[count.index].id]
+  vm_size               = "Standard_DS1_v2"
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
 }
