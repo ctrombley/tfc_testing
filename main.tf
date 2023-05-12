97018db083d5a2e05d5674493d86814275923dfd
Chris Trombley <chris.trombley@hashicorp.com>
2023-05-12 11:12:35 -0700

    checkin

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 terraform {
-  required_providers {
+  cloud {
+    organization = "trombs-test-org"
+    hostname = "app.staging.terraform.io"
+
+    workspaces {
+      name = "tfc_testing"
+    }
+  }
+    required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">=3.46.0"
@@ -7,13 +15,16 @@ terraform {
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
 
-locals {
-  tags = { Name = "drift-test" }
-  location = "southcentralus"
+variable "tags" {
+  type = map(string)
+  default = {
+    Name = "drift-test"
+  }
 }
 
 module "subnet_addrs" {
@@ -22,56 +33,61 @@ module "subnet_addrs" {
   base_cidr_block = "10.0.0.0/8"
   networks = [
     {
+      name     = "bastion"
+      new_bits = 4
+    },
+    {
+      name     = "dev"
+      new_bits = 4
+    },
+    {
+      name     = "qa"
+      new_bits = 4
+    },
+    {
       name     = "pub"
       new_bits = 4
     },
+    {
+      name     = "pd-frontend"
+      new_bits = 3
+    },
+    {
+      name     = "pd-backend"
+      new_bits = 3
+    },
+    {
+      name     = "expansion-1"
+      new_bits = 1
+    },
   ]
+}
+
+module "qco-vnet" {
+  source  = "./vnet"
+  # insert required variables here
+  tags     = var.tags
+  opu_code = "test"
+
+  # Network Resource Group
+  resource_group_name = azurerm_resource_group.vnet.name
+
+  # Vnet Information
+  vnet_address_space = [module.subnet_addrs.base_cidr_block]
+
+  vnet_dns_servers = ["10.0.0.4"]
+
+  #SNet Information
+  snet_bastion_address_prefixes     = [module.subnet_addrs.network_cidr_blocks["bastion"]]
+  snet_dv_address_prefixes          = [module.subnet_addrs.network_cidr_blocks["dev"]]
+  snet_qa_address_prefixes          = [module.subnet_addrs.network_cidr_blocks["qa"]]
+  snet_pub_address_prefixes         = [module.subnet_addrs.network_cidr_blocks["pub"]]
+  snet_pd_frontend_address_prefixes = [module.subnet_addrs.network_cidr_blocks["pd-frontend"]]
+  snet_pd_backend_address_prefixes  = [module.subnet_addrs.network_cidr_blocks["pd-backend"]]
+  snet_exp_01_address_prefixes      = [module.subnet_addrs.network_cidr_blocks["expansion-1"]]
 }
 
 resource "azurerm_resource_group" "vnet" {
   name     = "example-resources"
   location = "West Europe"
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "test-vnet"
-  location            = local.location
-  resource_group_name = azurerm_resource_group.vnet.name
-  address_space       = ["10.0.0.0/8"]
-  dns_servers         = ["10.0.0.4"]
-  tags                = local.tags
-}
-
-resource "azurerm_subnet" "pub" {
-  name                                           = "test-pub-snet"
-  virtual_network_name                           = azurerm_virtual_network.vnet.name
-  resource_group_name                            = azurerm_resource_group.vnet.name
-  address_prefixes                               = [module.subnet_addrs.network_cidr_blocks["pub"]]
-  service_endpoints                              = []
-  enforce_private_link_endpoint_network_policies = false
-}
-resource "azurerm_network_security_group" "pub" {
-  name                = "test-pub"
-  location            = local.location
-  resource_group_name = azurerm_resource_group.vnet.name
-  tags                = local.tags
-}
-
-resource "azurerm_network_security_rule" "pub_deny" {
-  name                        = "Deny-VNet-out"
-  priority                    = 4000
-  direction                   = "Outbound"
-  access                      = "Deny"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "VirtualNetwork"
-  resource_group_name         = azurerm_resource_group.vnet.name
-  network_security_group_name = azurerm_network_security_group.pub.name
-}
-
-resource "azurerm_subnet_network_security_group_association" "pub" {
-  subnet_id                 = azurerm_subnet.pub.id
-  network_security_group_id = azurerm_network_security_group.pub.id
 }
