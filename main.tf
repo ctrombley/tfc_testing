3191ceaafe6e21374b7b3114dce80d77001ed5cf
Chris Trombley <chris.trombley@hashicorp.com>
2023-06-20 16:24:04 -0700

    test

--- main.tf
+++ main.tf
@@ -9,6 +9,36 @@ terraform {
     }
   }
 }
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+}
+
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = module.vpc.private_subnet_cidr_blocks
+}
+
+module "key_pair" {
+  source = "terraform-aws-modules/key-pair/aws"
+
+  key_name           = "deployer-one"
+  create_private_key = true
+}
 
 data "hcp_packer_image" "learn-packer_image" {
   bucket_name     = "learn-packer"
@@ -19,11 +49,11 @@ data "hcp_packer_image" "learn-packer_image" {
 
 resource "aws_instance" "hashiapp" {
   ami                         = data.hcp_packer_image.learn-packer_image.cloud_image_id
-  instance_type               = var.instance_type
+  instance_type               = "t2.micro"
   associate_public_ip_address = true
-  subnet_id                   = aws_subnet.hashiapp.id
+  subnet_id                   = module.vpc.private_subnets[0]
   vpc_security_group_ids      = [aws_security_group.hashiapp.id]
-  key_name                    = aws_key_pair.generated_key.key_name
+  key_name                    = module.key_pair.key_pair_name
 
   lifecycle {
     postcondition {
