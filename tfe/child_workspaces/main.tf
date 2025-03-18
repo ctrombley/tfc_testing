28274d88ec658f967541705fe9f6a32a17f15fc7
Chris Trombley <chris.trombley@hashicorp.com>
2025-03-18 09:38:10 -0700

    update

--- tfe/child_workspaces/main.tf
+++ tfe/child_workspaces/main.tf
@@ -10,10 +10,10 @@ terraform {
 }
 
 provider "tfe" {
-  hostname = "app.terraform.io"
+  hostname = "tfcdev-781a2fe5.ngrok.io"
 }
 
-resource "tfe_project" "this" {
+data "tfe_project" "this" {
   organization = "hashicorp"
   name = "Experiments"
 }
@@ -22,7 +22,7 @@ resource "tfe_workspace" "child" {
   count        = 6
   organization = "hashicorp"
   name         = "child-${count.index}-${random_id.child_id.id}"
-  project_id = tfe_project.this.id
+  project_id = data.tfe_project.this.id
 }
 
 resource "random_id" "child_id" {
